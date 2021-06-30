bb8975ca89d46aa24503c16f584feb82d2b7f781
Efgon <egonzalezgar@gmail.com>
2021-06-30 14:43:20 -0400

    more changes for deployment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/SEres">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
